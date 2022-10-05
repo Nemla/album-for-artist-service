09dd99e3b506bab60276c416d87280ea8ed49d0c
Almen Novalic <almen.novalic@edu.edugrade.se>
2022-10-05 18:39:25 +0200

    first commit

---
+++ src/main/java/grupp4/edufy/albumforartistservice/AlbumForArtistServiceApplication.java
@@ -0,0 +1,13 @@
+package grupp4.edufy.albumforartistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumForArtistServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbumForArtistServiceApplication.class, args);
+    }
+
+}
